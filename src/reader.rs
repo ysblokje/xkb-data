d7817b50573569473326e232276dcd0f63cc1fa5
ysblokje <ysblokje@protonmail.ch>
2026-01-03 11:49:38 +0100

    read the full xml file(s) as well as parts

--- src/reader.rs
+++ src/reader.rs
@@ -5,8 +5,16 @@ use std::{
     io::{self, BufReader},
 };
 
+use crate::{layout::KeyboardLayouts, option::KeyboardOptions};
+
 /// Fetches a section from the xml file
 pub(crate) fn get_section<Section: for<'a> Deserialize<'a>>(path: &str) -> io::Result<Section> {
+    xml::from_reader(BufReader::new(File::open(path)?))
+        .map_err(|why| io::Error::new(io::ErrorKind::InvalidData, format!("{}", why)))
+}
+
+/// Fetch all known sections in one go
+pub fn read_config(path: &str) -> io::Result<(KeyboardLayouts, KeyboardOptions)> {
     xml::from_reader(BufReader::new(File::open(path)?))
         .map_err(|why| io::Error::new(io::ErrorKind::InvalidData, format!("{}", why)))
 }
